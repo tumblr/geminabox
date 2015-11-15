5eb1c93ed3e8e5199f472544a81bdf01204e8dcf
Tom Lea <commit@tomlea.co.uk>
2015-11-15 12:15:42 +0000

    Test coverage & validation for FilenameGenerator.

--- lib/geminabox/filename_generator.rb
+++ lib/geminabox/filename_generator.rb
@@ -1,7 +1,16 @@
 module Geminabox::FilenameGenerator
   def gem_filename(name, version, platform)
+    platform = String(platform)
+    name = String(name)
+    version = String(version)
+
+    raise ArgumentError, "platform is required" if platform.empty?
+    raise ArgumentError, "name is required" if name.empty?
+    raise ArgumentError, "version is required" if version.empty?
+
     platform_for_filename = platform unless platform == "ruby"
     filename_parts = [name, version, platform_for_filename].compact
+
     "#{filename_parts.join("-")}.gem"
   end
 
---
+++ spec/unit/filename_generator_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe Geminabox::FilenameGenerator do
+  include Geminabox::FilenameGenerator
+
+  describe "#gem_filename" do
+    context "with platform of ruby" do
+      it do
+        expect(gem_filename("foo", "1.2.3", "ruby"))
+          .to eq("foo-1.2.3.gem")
+      end
+    end
+
+    context "with platform of not jruby" do
+      it do
+        expect(gem_filename("foo", "1.2.3", "jruby"))
+          .to eq("foo-1.2.3-jruby.gem")
+      end
+    end
+
+    context "with platform of nil" do
+      it do
+        expect{ gem_filename("foo", "1.2.3", nil) }
+          .to raise_error(ArgumentError)
+      end
+    end
+
+    context "with version of nil" do
+      it do
+        expect{ gem_filename("foo", nil, "ruby") }
+          .to raise_error(ArgumentError)
+      end
+    end
+
+    context "with name of nil" do
+      it do
+        expect{ gem_filename(nil, "1.2.3", "ruby") }
+          .to raise_error(ArgumentError)
+      end
+    end
+  end
+end
